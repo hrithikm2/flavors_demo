4c890b9e253101558f2825a837208b9aaa06d390
hrithikmtorana <hrithik.m@toranainc.com>
2025-08-04 12:31:33 +0530

    Update launch.json

--- lib/environment/dev/main_dev.dart
+++ lib/environment/dev/main_dev.dart
@@ -1,12 +1,19 @@
 import 'dart:async';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flavors_demo/environment/dev/firebase_options_dev.dart';
 import 'package:flavors_demo/flavors.dart';
 import 'package:flavors_demo/main_common.dart';
 import 'package:flutter/services.dart';
 
 Future<void> main() async {
-  FlavorConfig.appFlavor = Flavor.values.firstWhere(
-    (element) => element.name == appFlavor,
+  FlavorConfig.appFlavor = Flavor.dev;
+
+
+
+  // TODO: Add Firebase configuration
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 
   await mainCommon();
